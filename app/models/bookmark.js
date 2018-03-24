c3be62dbd01692c502a39d77d987c065841f5587
Waihon Yew <yewwaihon@gmail.com>
2018-03-24 18:58:06 +0800

    Transform attributes of bookmark model

--- app/models/bookmark.js
+++ app/models/bookmark.js
@@ -2,9 +2,11 @@ import DS from 'ember-data';
 import attr from 'ember-data/attr';
 
 export default DS.Model.extend({
-  link: attr(),
-  title: attr(),
-  about: attr(),
-  public: attr(),
-  created: attr()
+  link: attr('string'),
+  title: attr('string'),
+  about: attr('string'),
+  public: attr('boolean'),
+  created: attr('date', {
+    defaultValue() { return new Date(); }
+  })
 });
