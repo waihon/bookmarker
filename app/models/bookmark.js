36eec815974eec9859c4da6e729c93e54d55ba13
Waihon Yew <yewwaihon@gmail.com>
2018-03-24 18:03:28 +0800

    Specify attributes of bookmark model

--- app/models/bookmark.js
+++ app/models/bookmark.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
+import attr from 'ember-data/attr';
 
 export default DS.Model.extend({
-
+  link: attr(),
+  title: attr(),
+  about: attr(),
+  public: attr(),
+  created: attr()
 });
