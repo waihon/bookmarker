a26d3980c2daac2bc5ec4619f26b51469b648d2b
Waihon Yew <yewwaihon@gmail.com>
2018-03-29 00:05:45 +0800

    Retrieve bookmarks from the store

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,26 +1,7 @@
 import Route from '@ember/routing/route';
 
-let bookmarks = [{
-  id: 1,
-  title: 'Microsoft',
-  link: 'https://www.microsoft.com/',
-  about: 'At Microsoft our mission and values are to help people and business throughout the world realize their full potential.'
-},
-{
-  id: 2,
-  title: 'Wikipedia',
-  link: 'https://en.wikipedia.org/wiki/Main_Page',
-  about: 'The Free Encyclopedia'
-},
-{
-  id: 3,
-  title: 'Google',
-  link: 'https://www.google.com/',
-  about: null
-}];
-
 export default Route.extend({
   model() {
-    return bookmarks;
+    return this.get('store').findAll('bookmark');
   }
 });
