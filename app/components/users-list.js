0b6492d29ebc1860175e6efd3becee74cce84622
Waihon Yew <yewwaihon@gmail.com>
2018-03-29 23:01:38 +0800

    Add a click event to users-list

--- app/components/users-list.js
+++ app/components/users-list.js
@@ -1,7 +1,11 @@
 import Component from '@ember/component';
 
 const UsersListComponent = Component.extend({
-  imgClass: "avatar"
+  imgClass: "avatar",
+  click() {
+    Ember.Logger.info('users-list was clicked');
+    return false; // stop the event from bubbling up
+  }
 });
 
 UsersListComponent.reopenClass({
